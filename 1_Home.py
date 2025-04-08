1c2b372b38b618c3b2edc6bbf3910df0e87e222b
Tpz64 <thaispz64@gmail.com>
2025-04-07 21:16:56 -0300

    Atualizando home

--- 1_Home.py
+++ 1_Home.py
@@ -5,35 +5,35 @@ st.set_page_config(page_title="Produtividade construção", layout="wide")
 #info lateral da equipe
 st.sidebar.markdown("""
     <div style="
-        background-color: #ffff; 
+        background-color: #fffbf5; 
         color:white;
         padding: 9px; 
         border-radius: 10px;
         text-align: center;
     ">
-        <p style="color:black; font-weight: bold;">Desenvolvido por:</p>
+        <p style="color:black; font-weight:bold;">Desenvolvido por:</p>
         <div>
             <a href="https://www.linkedin.com" target="_blank" 
-               style="color: black; text-decoration: none; font-weight: bold;">
+               style="color: black; text-decoration: none;">
                 André de Sousa Neves
             </a>
         </div>
         <div>
             <a href="https://www.linkedin.com" target="_blank" 
-               style="color: black; text-decoration: none; font-weight: bold;">
+               style="color: black; text-decoration: none;">
                 Beatriz Dantas Sampaio
             </a>
         </div>
         <div>
             <a href="https://www.linkedin.com" target="_blank" 
-               style="color: black; text-decoration: none; font-weight: bold;">
+               style="color: black; text-decoration: none;">
                 Isabela Barcellos Freire
             </a>
         </div>
         <div>
             <a href="https://www.linkedin.com/in/thaisgleoncio/" target="_blank" 
-               style="color: black; text-decoration: none; font-weight: bold;">
-                Thaís Leoncio
+               style="color: black; text-decoration: none;">
+                Thaís Gonçalves Leoncio
             </a>
         </div>
     </div>
@@ -42,15 +42,18 @@ st.sidebar.markdown("""
 #Inicio análise
 #st.markdown("")
 st.header("Introdução do caso")
-st.write("Colocar texto inicial aqui. Descrição do problema ou contexto do mercado")
-st.write("A análise que será abordada neste estudo enfoca obras elétricas, com o objetivo de avaliar a qualidade de cada projeto, o tempo de execução e os materiais empregados. Esse processo busca aprimorar as previsões para futuras obras, otimizar a qualidade dos serviços prestados e compreender melhor o fluxo de desenvolvimento dessas obras.")
+st.write("A construção civil é um dos setores mais importantes para o desenvolvimento socioeconômico de um país, sendo responsável por obras de infraestrutura, habitação e urbanização. Seu impacto direto na geração de empregos, movimentação da economia e melhoria da qualidade de vida torna essencial o aprimoramento constante de suas práticas. Nesse contexto, a produtividade na construção civil se destaca como um dos principais indicadores de eficiência do setor.")
+st.write("O índice de produtividade na construção civil é utilizado para medir o desempenho das equipes ou dos processos executivos de uma obra, demonstrando quanto de determinado serviço é produzido em um intervalo de tempo com os recursos disponíveis. Sua fórmula básica é:")
+st.write("Produtividade = Quantidade Produzida / Recursos Utilizados")
+st.write("Esse índice permite aos gestores planejar com maior precisão, controlar custos, otimizar o uso de materiais e mão de obra, além de identificar gargalos nos processos construtivos. Com ele, é possível promover a melhoria contínua dos métodos de execução, resultando em obras mais rápidas, econômicas e sustentáveis.")
+st.write("Neste dashboard iremos analisar uma base de dados já tratada que foi utilizada para a atualização da tabela SIURB, uma tabela que é desenvolvida pela Secretaria Municipal de Infraestrutura Urbana e Obras, que reúne composições de custos unitários de serviços, preços de insumos e índices de produtividade da mão de obra, sendo utilizada como referência obrigatória em orçamentos de obras públicas municipais.")
 
-st.markdown("""
-Parte 0 – Introdução ao Problema e à Base de Dados (obrigatório)
-- Apresentação clara do problema ou contexto de mercado
-- Descrição da base de dados e variáveis
-- Identificação de perguntas de análise e possíveis hipóteses
-- Conexão entre o problema real e o que será desenvolvido no
-dashboard
-- Organização inicial do layout e estrutura do dashboard
-""")
+#Aqui colocar em colunas/ de um lado 
+col1, col2 = st.columns([2,2])
+
+with col1:
+    st.write("A análise que será abordada neste estudo tem como foco obras elétricas, com o objetivo de avaliar a qualidade de cada projeto, o tempo de execução e os materiais empregados. Esse processo busca aprimorar as previsões para futuras obras, otimizar a qualidade dos serviços prestados e compreender melhor o fluxo de desenvolvimento dessas obras.")
+
+with col2: 
+    st.image("imgs/img-instalacao-eletrica.jpg",width=400)
+
--- pages/2_Análise Inicial.py
+++ pages/2_Análise Inicial.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import time
 
 #info lateral da equipe
 st.sidebar.markdown("""
@@ -39,6 +40,23 @@ st.sidebar.markdown("""
 
 #titulo da página
 st.header("Analise Inicial dos dados")
+
+#Barra de carregamento
+my_bar = st.progress(0)
+for percent_complete in range(100):
+     time.sleep(0.05)
+     my_bar.progress(percent_complete + 1)
+
+st.write("Os dados que temos vieram daqui.")
+st.write("Os dados que usaremos para essa análise são esses")
+st.write("Colocar aqui as variaveis comentadas")
+
+st.write("Perguntas de analise e possíveis hipoteses")
+
+st.write("Conexão entre o problema real e o que será desenvolvido no dashboard -> lembrando que a base de dados que temos é apenas uma amostra. ")
+
+
+
 st.write("textinho inicial aqui. Apresentação do dataset e suas variáveis")
 
 st.write("Apresentação do dataset e suas variáveis")
@@ -46,3 +64,14 @@ st.write("Apresentação do dataset e suas variáveis")
 st.write("Identificação de possíveis hipóteses e perguntas investigativas")
 
 st.write("Preparação do ambiente inicial do dashboard (layout e estrutura)")
+
+st.markdown("""
+            ---------
+Parte 0 – Introdução ao Problema e à Base de Dados (obrigatório)
+- Apresentação clara do problema ou contexto de mercado
+- Descrição da base de dados e variáveis
+- Identificação de perguntas de análise e possíveis hipóteses
+- Conexão entre o problema real e o que será desenvolvido no
+dashboard
+- Organização inicial do layout e estrutura do dashboard
+""")
